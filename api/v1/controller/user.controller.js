474c19d0d400713748a0528e1e123a7dc2923a40
Truong Van Quy <truongvanquy2k3@gmail.com>
2024-02-14 13:13:42 +0700

    add forgot password feature

--- api/v1/controller/user.controller.js
+++ api/v1/controller/user.controller.js
@@ -1,7 +1,9 @@
 const User = require('../models/user.model')
+const ForgotPassword = require('../models/forgot-password.model')
 
 // helpers
 const generateHelper = require('../../../helpers/generate.helper')
+const sendEmailHelper = require('../../../helpers/sendEmail.helper')
 
 // hash password with md5
 const md5 = require('md5')
@@ -72,7 +74,129 @@ module.exports.login = async (req, res) => {
         console.log('Error occured:', error);
         res.json({
             code: 400,
-            message: 'An error occured while logining the account'
+            message: 'An error occured while logining to the account'
+        });
+    }
+}
+
+// [POST] /api/v1/users/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+    try {
+        const email = req.body.email
+
+        const user = await User.findOne({ email: email, deleted: false })
+
+        if (!user) {
+            res.json({
+                code: 400,
+                message: 'Email does not existed',
+            })
+            return
+        }
+
+        // save otp data to forgot-password model
+        const expireTime = 3
+        const otp = generateHelper.generateRandomNumber(6)
+        const forgotPasswordObject = {
+            email: email,
+            otp: otp,
+            expireAt: Date.now() + expireTime * 60 * 1000
+        }
+        const forgotPassword = new ForgotPassword(forgotPasswordObject)
+        await forgotPassword.save()
+
+        // send otp to user's email
+        const emailSubject = 'Mã OTP xác minh lấy lại mật khẩu'
+        const emailContentHtml = `
+            Mã OTP xác minh lấy lại mật khẩu là <b>${otp}</b>. Thời hạn sử dụng là ${expireTime} phút. Lưu ý không được để lộ mã OTP. 
+        `
+        sendEmailHelper.sendEmail(user.email, emailSubject, emailContentHtml)
+
+        res.json({
+            code: 200,
+            message: 'Send opt code to user email successfully',
+        })
+    } catch (error) {
+        console.log('Error occured:', error);
+        res.json({
+            code: 400,
+            message: 'An error occured while implement forgetten password procedure'
+        });
+    }
+}
+
+// [POST] /api/v1/users/password/otp
+module.exports.otpPassword = async (req, res) => {
+    try {
+        const email = req.body.email
+        const otp = req.body.otp
+
+        const forgotPassword = await ForgotPassword.findOne({
+            email: email,
+            otp: otp    
+        })
+
+        if (!forgotPassword) {
+            res.json({
+                code: 400,
+                message: 'Email or otp code is not valid',
+            })
+            return
+        }
+
+        const user = await User.findOne({
+            email: email
+        })
+
+        const token = user.token
+        res.cookie('token', token)
+        
+        res.json({
+            code: 200,
+            message: 'Successful authentication',
+            token: token
+        })
+    } catch (error) {
+        console.log('Error occured:', error);
+        res.json({
+            code: 400,
+            message: 'An error occured while retriving email and otp'
+        });
+    }
+}
+
+// [POST] /api/v1/users/password/reset
+module.exports.resetPassword = async (req, res) => {
+    try {
+        const token = req.cookies.token
+        const password = req.body.password
+
+        const user = await User.findOne({
+            token: token
+        })
+
+        if (!user) {
+            res.json({
+                code: 400,
+                message: 'User not found',
+            })
+        }
+
+        await User.updateOne({
+            token: token
+        }, {
+            password: md5(password)
+        })
+        
+        res.json({
+            code: 200,
+            message: 'Change password successfully',
+        })
+    } catch (error) {
+        console.log('Error occured:', error);
+        res.json({
+            code: 400,
+            message: 'An error occured while retriving email and otp'
         });
     }
 }
